fa3191d2d1768eaeeab6e8a7cace73af77b0070b
NikolenkoRostislav <rostislavnikolenkowork@gmail.com>
2025-09-01 00:12:12 +0200

    refactor: updated imports in main.py

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import os
-from rembg import remove
+print("importing rembg")
+import rembg
+print("import successful")
 from PIL import Image
 
 INPUT_DIR = "input"
@@ -10,6 +12,7 @@ os.makedirs(OUTPUT_DIR, exist_ok=True)
 
 def remove_bgs():
     for filename in os.listdir(INPUT_DIR):
+        print(filename)
         input_path = os.path.join(INPUT_DIR, filename)
         output_path = os.path.join(OUTPUT_DIR, filename)
 
@@ -20,14 +23,14 @@ def remove_bgs():
             continue
 
         try:
-            output_img = remove(input_img, 
+            output_img = rembg.remove(input_img, 
                 alpha_matting=True, alpha_matting_foreground_threshold=270,alpha_matting_background_threshold=20, alpha_matting_erode_size=11, 
                 post_process_mask=True
             )
         except Exception:
             print(f"failed to remove background from {filename} with post processing")
             try:
-                output_img = remove(input_img)
+                output_img = rembg.remove(input_img)
             except Exception:
                 print(f"failed to remove background from {filename}")  
                 continue
